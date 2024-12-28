dd7907615ff9ac647224f098bf9b2a7749c1a233
chamchiking <chamj61047@snu.ac.kr>
2024-12-28 22:35:58 +0900

    feat: added host code for Dual DVS but need to check the Frame Baseaddresses and merge the host codes

---
+++ 2.Dual_DVS/host/src/bbox.hpp
@@ -0,0 +1,17 @@
+#ifndef BBOX_HPP
+#define BBOX_HPP
+/**
+* struct to hold x and y coordinates of bounding box vertices.
+* @param lx x coordinate of leftmost edge
+* @param ly y coordinate of topmost edge
+* @param hx x coordinate of rightmost edge
+* @param hy y coordinate of bottommost edge 
+*/
+typedef struct
+{
+    int lx;
+    int ly;
+    int hx;
+    int hy;
+} Bbox;
+#endif
