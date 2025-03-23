93b23687043cd5b04bcf996b54e2c3c1dcb305db
chamchiking <chamj61047@snu.ac.kr>
2025-03-23 18:48:03 +0900

    algorithm branch added

---
+++ algorithm/src/main.cpp
@@ -0,0 +1,93 @@
+
+#include <thread>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <cassert>
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <dirent.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+using namespace cv;
+using namespace std;
+
+enum Mode
+{
+    DVS_ROI_AVG_BASE = 1,
+    DVS_ROI_PROPOSED
+};
+
+void handleMode(Mode mode);
+Mode parseArguments(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+    Mode mode = parseArguments(argc, argv);
+
+    handleMode(mode);
+
+    return 0;
+}
+
+void handleMode(Mode mode)
+{
+    switch(mode)
+    {
+        case DVS_ROI_AVG_BASE:
+            printf("DVS ROI AVG mode\n");
+
+            break;
+        case DVS_ROI_PROPOSED:
+            printf("DVS ROI PROPOSED mode\n");
+            
+            break;
+        default:
+            fprintf(stderr, "Error: Unknown mode \n");
+            exit(EXIT_FAILURE);
+    }
+}
+
+
+Mode parseArguments(int argc, char *argv[])
+{
+    Mode mode = DVS_ROI_PROPOSED; // Default mode
+
+    // Define command-line options
+    struct option long_options[] = {
+        {"roi_avg", no_argument, nullptr, 'a'},
+        {"roi_proposed", no_argument, nullptr, 'p'},
+        {nullptr, 0, nullptr, 0}};
+
+    // Parse command-line arguments
+    int opt;
+    while ((opt = getopt_long(argc, argv, "ap", long_options, nullptr)) != -1)
+    {
+        switch (opt)
+        {
+        case 'a':
+            // average based DVS ROI algorithm
+            mode = DVS_ROI_AVG_BASE;
+            break;
+        case 'p':
+            // proposed ROI algorithm
+            mode = DVS_ROI_PROPOSED;
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [--roi_avg | --roi_proposed]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return mode;
+}
