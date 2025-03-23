20f1a9873d7b5f2476a1b9e23b86f3f10d2bdb82
lkh099 <lkh099@snu.ac.kr>
2025-03-23 16:57:41 +0900

    restored CIS_DVS host functionality

--- pcie_setup/pcie_install/run_root.sh
+++ pcie_setup/pcie_install/run_root.sh
@@ -1,3 +1,3 @@
 ROOT_PWD="9Daffodils!"
 echo $ROOT_PWD | sudo -S -i
-sudo sh /home/cappfpga01/src/2.mccha/XVCU_TRD/host/pcie_install/start_pcie.sh
+sudo sh /home/nrvfpga01/xsrc/embeddedsw/pcie_setup/pcie_install/start_pcie.sh
